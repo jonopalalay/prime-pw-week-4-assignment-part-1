fb3df1659ec5de82fe59a34a2244d06f0e55f4ff
Jonathan Palalay <jonopalalay@gmail.com>
2022-02-13 14:01:45 -0600

    Completed assignment 1

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -137,12 +137,32 @@ function inputArray(positiveArray){
     if(positiveArray[i] > 0)
       firstArray.push(positiveArray[i]);
     }
+    return firstArray;
   }
-  return firstArray;
-}
+
+  console.log(inputArray([1,3,5]));
+  console.log(inputArray([-1,-3,-5]))
+
+
 
 
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or
 //     CodeWars(https://www.codewars.com/). Then describe it
 //     here in a comment, write the function, and test it!
+// Create a function that takes an array containing only numbers and return the first element.
+
+// getFirstValue([1, 2, 3]) ➞ 1
+//
+// getFirstValue([80, 5, 100]) ➞ 80
+//
+// getFirstValue([-500, 0, 50]) ➞ -500
+
+function getFirstValue(arr) {
+	let first = arr[0];
+  return (first);
+}
+
+console.log('First element of the array is 1', getFirstValue([1, 2, 3]));
+console.log('First element of the array is 80', getFirstValue([80, 5, 100]));
+console.log('First element of the array is -500', getFirstValue([-500, 0, 50]));
