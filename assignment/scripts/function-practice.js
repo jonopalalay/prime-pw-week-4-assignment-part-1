ecce3b8e824caf21a11b6b35768958e03034bb87
Jonathan Palalay <jonopalalay@gmail.com>
2022-02-12 18:12:56 -0600

    finihed part way stretch questions

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -72,7 +72,7 @@ function getLast( array ) {
   if (array.length > 0) {
     return array[array.length - 1];
     }
-    return undefined
+    return undefined;
 
   // return (array.length === 0) ? undefined : array.pop();
 }
@@ -92,9 +92,11 @@ function find( value, array ){
       return true
     }
       return false
+    }
 }
 
-console.log(find( 1,[1,2,3] )
+console.log(find( 1,[1,2,3]) );
+console.log(find( 3,[1,2,5]) );
 
 
 
@@ -105,21 +107,39 @@ console.log(find( 1,[1,2,3] )
 // 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+  if (letter === string.charAt(0)){
+    return true;
+  }
+    return false;
 }
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
-  let sum = 0
+function sumAll(numbers) {
+  let sum = 0;
+  for(let i=0; i<numbers.length; i++){
+  sum+=numbers[i];
+}
   // TODO: loop to add items
   return sum;
+
 }
 
+console.log('The sum of all numbers', sumAll([1,3,4,5]));
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
+
+function inputArray(positiveArray){
+  let firstArray = [];
+  for (let i=0; i<positiveArray.length; i++){
+    if(positiveArray[i] > 0)
+      firstArray.push(positiveArray[i]);
+    }
+  }
+  return firstArray;
+}
 
 
 
